73ddef80a75fb0635a2aa78a5417d6f63e94fd95
Sergi Mayol <sergi2808@gmail.com>
2022-03-06 14:37:36 +0100

    bloques.c y bloques.h
    bloques.c/.h finalizados en cuanto al nivel 1 casi seguro

--- bloques.c
+++ bloques.c
@@ -1,1 +1,98 @@
 #include "bloques.h"
+
+// Descriptor del fichero alamcenado como variable global estática,
+// descpara que sólo pueda ser accedida en bloques.c
+static int descriptor = 0;
+
+// Función para montar el dispositivo virtual, y dado que se
+// trata de un fichero, esa acción consistirá en abrirlo.
+int bmount(const char *camino)
+{
+    //! NO se si hay que comprobar si hay un fichero ya abierto
+    if (descriptor > 0)
+    {
+        close(descriptor);
+    }
+    // Permisos rw-rw-rw (6-6-6)
+    descriptor = open(camino, O_RDWR | O_CREAT, 0666);
+    if (descriptor == -1)
+    {
+        // error (return -1)
+        return EXIT_FAILURE;
+    }
+    return descriptor;
+}
+
+// Desmonta el dispositivo virtual. Devuelve 0 (o EXIT_SUCCESS)
+// si se ha cerrado el fichero correctamente, o -1 (o EXIT_FAILURE)
+// en caso contrario.
+int bmount()
+{
+    descriptor = close(descriptor);
+    if (descriptor == -1)
+    {
+        // return -1
+        return EXIT_FAILURE;
+    }
+    // return 0
+    return EXIT_SUCCESS;
+}
+
+// Escribe 1 bloque en el dispositivo virtual,
+// en el bloque físico especificado por nbloque.
+int bwrite(unsigned int nbloque, const void *buf)
+{
+    // lseek: despplazamiento en el gestor de ficheros al bloque deseado
+    // SEEK_SET = punto de referencia, desde el inicio del fichero
+    off_t desp = lseek(descriptor, nbloque * BLOCKSIZE, SEEK_SET);
+    if (desp == -1)
+    {
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        //! Optimizable, de momento no optimizar para una mayor comprensión
+        size_t bloquesW = write(descriptor, buf, BLOCKSIZE);
+        // devuelve el nº de bytes que ha podido escribir
+        //(si ha ido bien, será BLOCKSIZE), o -1 (o EXIT_FAILURE) si se produce un error.
+        if (bloquesW == -1)
+        {
+            // Se ha producido un error al escribir
+            return EXIT_FAILURE;
+        }
+        else
+        {
+            // Se deveulven el num de bloques escritos
+            return BLOCKSIZE;
+        }
+    }
+}
+
+// Lee 1 bloque del dispositivo virtual, que se corresponde
+// con el bloque físico especificado por nbloque.
+int bread(unsigned int nbloque, void *buf)
+{
+    off_t desp = lseek(descriptor, nbloque * BLOCKSIZE, SEEK_SET);
+    // lseek: despplazamiento en el gestor de ficheros al bloque deseado
+    // SEEK_SET = punto de referencia, desde el inicio del fichero
+    off_t desp = lseek(descriptor, nbloque * BLOCKSIZE, SEEK_SET);
+    if (desp == -1)
+    {
+        return EXIT_FAILURE;
+    }
+    else
+    {
+        //! Optimizable, de momento no optimizar para una mayor comprensión
+        size_t bloqueL = read(descriptor, buf, BLOCKSIZE);
+        if (bloqueL == -1)
+        {
+            // Se ha producido un error al escribir
+            return EXIT_FAILURE;
+        }
+        else
+        {
+            // Se deveulven el num de bloques leidos
+            return BLOCKSIZE;
+        }
+    }
+}
--- bloques.h
+++ bloques.h
@@ -1,15 +1,16 @@
 // bloques.h
- 
-#include <stdio.h>  //printf(), fprintf(), stderr, stdout, stdin
-#include <fcntl.h> //O_WRONLY, O_CREAT, O_TRUNC
-#include <sys/stat.h> //S_IRUSR, S_IWUSR
-#include <stdlib.h>  //exit(), EXIT_SUCCESS, EXIT_FAILURE, atoi()
-#include <unistd.h> // SEEK_SET, read(), write(), open(), close(), lseek()
-#include <errno.h>  //errno
-#include <string.h> // strerror()
- 
-#define BLOCKSIZE 1024 // bytes
- 
+
+#include <stdio.h>      //printf(), fprintf(), stderr, stdout, stdin
+#include <fcntl.h>      //O_WRONLY, O_CREAT, O_TRUNC
+#include <sys/stat.h>   //S_IRUSR, S_IWUSR
+#include <stdlib.h>     //exit(), EXIT_SUCCESS, EXIT_FAILURE, atoi()
+#include <unistd.h>     // SEEK_SET, read(), write(), open(), close(), lseek()
+#include <errno.h>      //errno
+#include <string.h>     // strerror()
+
+#define BLOCKSIZE 1024  // bytes
+#define EXIT_FAILURE -1 // Redefinir EXIT_FAILURE a -1
+
 int bmount(const char *camino);
 int bumount();
 int bwrite(unsigned int nbloque, const void *buf);
