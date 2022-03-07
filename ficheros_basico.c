2b9110518ef0278524b823e6e51f6118c7c2f1bc
Sergi Mayol <sergi2808@gmail.com>
2022-03-07 10:14:55 +0100

    Update ficheros_basico.c

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -1,13 +1,63 @@
 #include "ficheros_basico.h"
 
+struct superbloque SB;
+
+// Calcula el tamaño en bloques necesario para el mapa de bits.
 int tamMB(unsigned int nbloques)
 {
-    
+    if ((nbloques / 8) % BLOCKSIZE != 0)
+    {
+        return ((nbloques / 8) / BLOCKSIZE) + 1;
+    }
+    else
+    {
+        return ((nbloques / 8) / BLOCKSIZE);
+    }
 }
 
-int tamAI(unsigned int ninodos) {}
+// Calcula el tamaño en bloques del array de inodos.
+int tamAI(unsigned int ninodos)
+{
+    if ((ninodos * INODOSIZE) % BLOCKSIZE != 0)
+    {
+        return ((ninodos * INODOSIZE) / BLOCKSIZE) + 1;
+    }
+    else
+    {
+        return ((ninodos * INODOSIZE) / BLOCKSIZE);
+    }
+}
 
-int initSB(unsigned int nbloques, unsigned int ninodos) {}
+// Inicializa los datos del superbloque.
+int initSB(unsigned int nbloques, unsigned int ninodos)
+{
+    //Posición del primer bloque del mapa de bits  
+    SB.posPrimerBloqueMB = posSB + tamSB;
+    //Posición del último bloque del mapa de bits 
+    SB.posUltimoBloqueMB = SB.posPrimerBloqueMB + tamMB(nbloques) - 1;
+    //Posición del primer bloque del array de inodos 
+    SB.posPrimerBloqueAI = SB.posUltimoBloqueMB + 1;
+    //Posición del último bloque del array de inodos  
+    SB.posUltimoBloqueAI = SB.posPrimerBloqueAI + tamAI(ninodos) - 1;
+    //Posición del primer bloque de datos 
+    SB.posPrimerBloqueDatos = SB.posUltimoBloqueAI + 1;
+    //Posición del último bloque de datos 
+    SB.posUltimoBloqueDatos = nbloques - 1;
+    //Posición del inodo del directorio raíz en el array de inodos
+    SB.posInodoRaiz = 0;
+    //Posición del primer inodo libre en el array de inodos
+    SB.posPrimerInodoLibre = 0;
+    //Cantidad de bloques libres en el SF
+    SB.cantBloquesLibres = nbloques;
+    //Cantidad de inodos libres en el array de inodos
+    SB.cantInodosLibres = ninodos;
+    //Cantidad total de bloques
+    SB.totBloques = nbloques;
+    //Cantidad total de inodos
+    SB.totInodos = ninodos;
+    //Escritura estructura bloque
+    return bwrite(posSB, &SB);
+}
 
 int initMB() {}
 
