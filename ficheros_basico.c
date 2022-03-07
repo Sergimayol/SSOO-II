b8349092da8d4fab25bc03f1baaab8f2400768fe
Sergi Mayol <sergi2808@gmail.com>
2022-03-07 13:09:33 +0100

    updated

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -31,34 +31,57 @@ int tamAI(unsigned int ninodos)
 // Inicializa los datos del superbloque.
 int initSB(unsigned int nbloques, unsigned int ninodos)
 {
-    //Posición del primer bloque del mapa de bits  
+    // Posición del primer bloque del mapa de bits
     SB.posPrimerBloqueMB = posSB + tamSB;
-    //Posición del último bloque del mapa de bits 
+    // Posición del último bloque del mapa de bits
     SB.posUltimoBloqueMB = SB.posPrimerBloqueMB + tamMB(nbloques) - 1;
-    //Posición del primer bloque del array de inodos 
+    // Posición del primer bloque del array de inodos
     SB.posPrimerBloqueAI = SB.posUltimoBloqueMB + 1;
-    //Posición del último bloque del array de inodos  
+    // Posición del último bloque del array de inodos
     SB.posUltimoBloqueAI = SB.posPrimerBloqueAI + tamAI(ninodos) - 1;
-    //Posición del primer bloque de datos 
+    // Posición del primer bloque de datos
     SB.posPrimerBloqueDatos = SB.posUltimoBloqueAI + 1;
-    //Posición del último bloque de datos 
+    // Posición del último bloque de datos
     SB.posUltimoBloqueDatos = nbloques - 1;
-    //Posición del inodo del directorio raíz en el array de inodos
+    // Posición del inodo del directorio raíz en el array de inodos
     SB.posInodoRaiz = 0;
-    //Posición del primer inodo libre en el array de inodos
+    // Posición del primer inodo libre en el array de inodos
     SB.posPrimerInodoLibre = 0;
-    //Cantidad de bloques libres en el SF
+    // Cantidad de bloques libres en el SF
     SB.cantBloquesLibres = nbloques;
-    //Cantidad de inodos libres en el array de inodos
+    // Cantidad de inodos libres en el array de inodos
     SB.cantInodosLibres = ninodos;
-    //Cantidad total de bloques
+    // Cantidad total de bloques
     SB.totBloques = nbloques;
-    //Cantidad total de inodos
+    // Cantidad total de inodos
     SB.totInodos = ninodos;
-    //Escritura estructura bloque
+    // Escritura estructura bloque
     return bwrite(posSB, &SB);
 }
 
-int initMB() {}
+// Inicializa el mapa de bits. En este nivel,de momento,
+// simplemente pondremos a 0 todos los bits del mapa de bits.
+int initMB()
+{
+    unsigned char buffer[BLOCKSIZE];
+    if (memset(buffer, 0, sizeof(buffer)) == NULL)
+    {
+        // error
+        return -1;
+    }
+    for (size_t i = SB.posPrimerBloqueMB; i < SB.posUltimoBloqueMB; i++)
+    {
+        if (bwrite(i, buffer) == -1)
+        {
+            // Error
+            return -1;
+        }
+    }
+    //! Creo que no finalizado
+    //? Reservar bloques¿?
+}
 
-int initAI() {}
+// Esta función se encargará de inicializar la lista de inodos libres.
+int initAI() {
+
+}
