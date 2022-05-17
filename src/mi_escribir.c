ddcc875c1e15fa2a6a69a42f10392a740f55f0c6
Alejandro Rodríguez <alerodriargui@gmail.com>
2022-05-17 10:01:07 +0200

    Update mi_escribir.c

--- src/mi_escribir.c
+++ src/mi_escribir.c
@@ -8,6 +8,39 @@ Miembros:
 
 int main(int argc, char **argv)
 {
+    if (argc != 5)
+    {
+        fprintf(stderr, "Sintaxis: ./mi_escribir <disco> </ruta_fichero> <texto> <offset>\n");
+        return -1;
+    }
 
-    return 0;
+    int pathL = strlen(argv[2]); // Ruta del fichero
+
+    if (argv[2][pathL - 1] != '/')
+    { // Es un fichero
+
+        if (bmount(argv[1]) == -1)
+        {
+            fprintf(stderr, "Error while mounting\n");
+            return -1;
+        }
+        char *camino = argv[2];
+        char *buffer_texto = argv[3];
+        unsigned int offset = atoi(argv[4]);
+        // Cantidad de bytes escritos
+        int nbytes = mi_write(camino, buffer_texto, offset, strlen(buffer_texto));
+
+        if (nbytes == -1)
+        {
+            nbytes = 0;
+        }
+        bumount();
+        fprintf(stderr, "Se han escrito %d bytes\n", nbytes);
+        return 0;
+    }
+    else // No es un fichero
+    {
+        fprintf(stderr, "No es un fichero\n");
+        return -1;
+    }
 }
